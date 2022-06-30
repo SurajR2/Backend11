2a392d010a946e5c48aca0452ed06f9474493156
SurajR2 <Surajrasaili190@gmail.com>
2022-06-30 16:43:00 +0545

    working login and register

--- router.js
+++ router.js
@@ -6,22 +6,44 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = require("./db-config");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { response, request } = require("express");
+const { response, request, query } = require("express");
 
 app.get("/login", (req, res) => {
     res.send({ message: "Hello" });
+});
+app.get("/register", (req, res) => {
+    res.send({ message: "working" });
+});
+app.post("/register", function (request, response) {
+    let fullname = request.body.fullname;
+    let username = request.body.username;
+    let email = request.body.email;
+    let password = request.body.password;
+    db.query(
+        "insert into register_user(fullname,username,email, password) values (?,?,?,?)",
+        [fullname, username, email, password],
+        function (err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(result);
+                response.status(200).send("registered successfully");
+            }
+            response.end();
+        }
+    );
+    console.log(request.body);
 });
 app.post("/login", function (request, response) {
     // Capture the input fields
     let email = request.body.email;
     let password = request.body.password;
-    let username = request.body.username;
     // Ensure the input fields exists and are not empty
     console.log(request.body);
     if (email && password) {
         // Execute SQL query that'll select the account from the database based on the specified email and password
         db.query(
-            "SELECT email, password FROM users WHERE email = ? AND password = ?",
+            "SELECT email, password FROM register_user WHERE email = ? AND password = ?",
             [email, password],
             function (error, results, fields) {
                 // If there is an issue with the query, output the error
@@ -44,22 +66,4 @@ app.post("/login", function (request, response) {
     }
 });
 
-// app.post("/login", (req, res) => {
-//     var email = request.body.email;
-//     var password = request.body.password;
-//     db.query(
-//         "SELECT email, password FROM userlogin where email = ? and password =? ",
-//         [email, password],
-//         function (error, results, fields) {
-//             if (error) throw error;
-//             if (results.length > 0) {
-//                 req.session.loggedin = true;
-//                 req.session.email = email;
-//                 return res.status(200).send({
-//                     msg: "Logged in successfully",
-//                 });
-//             }
-//         }
-//     );
-// });
 module.exports = app;
