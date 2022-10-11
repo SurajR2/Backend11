c89edd63ab5548852d7003a1b3675e97241bfbca
Atit-rakhal <101327409+Atit-rakhal@users.noreply.github.com>
2022-10-11 16:16:56 +0545

    verify token middleware added

---
+++ middleware.js
@@ -0,0 +1,30 @@
+const { request } = require('express');
+const express = require('express');
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
+
+const verifytoken = async (req, res, next) => {
+    const token = req.body.token || req.query.token || req.headers["token"] || req.params.token; 
+    
+
+    if(!token)
+    {
+        res.status(400).send({sucess:false,message:"Token is required for authentication" });
+        return;
+    }
+    try {
+   decoded=await jwt.verify(token,process.env.JWT_TOKEN);
+    const payload=(JSON.parse(JSON.stringify(decoded,null,2)));
+    console.log(`emle:`+payload.email);
+} catch (error) {
+        res.status(400).send({sucess:false,message:error.message});
+
+        
+    }
+    
+     return next();
+     
+
+};
+module.exports = verifytoken;
